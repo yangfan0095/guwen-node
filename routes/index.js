633901cbb95cd71084d6652118c8eab3a333db63
yangfan0095 <1056947355@qq.com>
2017-12-26 18:50:22 +0800

    删除冗余

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,6 @@ const bookActions = require('../controller/book')
 const book = require('../dbhelper/book')
 const booklist = require('../model/mongo').booklist;
 
-router.get('/', async(ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
-
-router.get('/string', async(ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
-router.get('/json', async(ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
-})
 router.get('/booklist', bookActions.booklist);
 router.get('/book', bookActions.queryBook);
 router.get('/chapter', bookActions.queryBookChapter);
